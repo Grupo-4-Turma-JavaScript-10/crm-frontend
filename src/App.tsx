6c2d5240e4f6e30586986ecbc7e6937c4bf2bf8b
Lancellot <assis.neto@rede.ulbra.br>
2026-01-28 18:46:01 -0300

    feat(borda): borda no navbar

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import { BrowserRouter, Routes } from 'react-router-dom'
 import './App.css'
 import { ToastContainer } from 'react-toastify'
 import 'react-toastify/dist/ReactToastify.css';
+import Navbar from './components/navbar/Navbar';
 
 function App() {
 
@@ -9,7 +10,7 @@ function App() {
     <>
       <ToastContainer />
       <BrowserRouter>
-        
+      <Navbar />
         <main>
           <Routes>
 
